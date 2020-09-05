e69df06acae82187564e893ca8ecbf3db7dd8a19
Anastasia Nekrasova <ejller@bk.ru>
2020-09-05 10:56:10 +0300

    Add env

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "c9155859d90d239f909d2906233816b26cd8cf5ede44702d422667672b58b0cd";
+const API_KEY = process.env.API_KEY;
 const BASE_API = "https://changenow.io/api/v1";
 
 export default {
